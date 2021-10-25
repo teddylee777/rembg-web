0bba351a49fa162e73173ef44a3b0d252114e0b1
teddylee777 <teddylee777@gmail.com>
2021-10-25 23:07:06 +0900

    주식 실험실 업데이트

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, Response
 from flask.helpers import send_file
 from werkzeug.utils import secure_filename
+
+from io import StringIO, BytesIO
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+import datetime
 import os
 import codecs
 import json
-from io import StringIO, BytesIO
-import datetime
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from mpl_finance import candlestick_ohlc
+
+import FinanceDataReader as fdr
+import pattern as pt
 
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = {'ipynb'}
@@ -25,10 +36,6 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def convert_file(filename_, settings):
-    # if filename_.endswith('.ipynb'):
-    #     data = folder + filename_ 
-    # else:
-    #     data = folder + filename_ + '.ipynb' 
     data = os.path.join(app.config['UPLOAD_FOLDER'], filename_)
     print(data)
     try:
@@ -136,10 +143,7 @@ def convert_file(filename_, settings):
 @app.route('/convert', methods=['GET', 'POST'])
 def convert():
     if request.method == 'POST':
-
-        # check if the post request has the file part
         if 'file' not in request.files:
-            # flash('No file part')
             return redirect(request.url)
         
         file = request.files['file']
@@ -172,6 +176,103 @@ def convert():
                      attachment_filename=f'{timestring}-{attach_filename}.md',# 다운받아지는 파일 이름. 
                      as_attachment=True)
     return render_template('convert.html')
+
+@app.route('/stock', methods=['GET', 'POST'])
+def stock():
+    if request.method == 'GET':
+        now_time = datetime.datetime.now()
+        current = now_time.strftime('%Y-%m-%d')
+        past = now_time - datetime.timedelta(20)
+        past = past.strftime('%Y-%m-%d')
+        return render_template('stock.html', startdate=past, enddate=current)
+    else:
+        code = request.form['code']
+        startdate = request.form['startdate']
+        enddate = request.form['enddate']
+        if request.form['action'] == '패턴검색':
+            return redirect(url_for('pattern', startdate=startdate, enddate=enddate, code=code))
+        elif request.form['action'] == '차트확인':
+            return render_template('stock.html', startdate=startdate, enddate=enddate, code=code, chart=True)
+
+@app.route('/plot.png', methods=['GET'])
+def plot_png():
+    code  = request.args.get('code', None)
+    startdate  = request.args.get('startdate', None)
+    enddate  = request.args.get('enddate', None)
+    print(startdate)
+    p = pt.PatternFinder()
+    p.set_stock(code)
+    result = p.search(startdate, enddate)
+    print(result)
+    if len(result) > 0:
+        fig = p.plot_pattern(list(result.keys())[0])
+        output = BytesIO()
+        FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route('/plotchart.png', methods=['GET'])
+def plot_chart():
+    code  = request.args.get('code', None)
+    startdate  = request.args.get('startdate', None)
+    enddate  = request.args.get('enddate', None)
+    
+    fig = plt.figure()
+    fig.set_facecolor('w')
+    gs = gridspec.GridSpec(2, 1, height_ratios=[3, 1])
+    axes = []
+    axes.append(plt.subplot(gs[0]))
+    axes.append(plt.subplot(gs[1], sharex=axes[0]))
+    axes[0].get_xaxis().set_visible(False)
+
+    print(code)
+    data = fdr.DataReader(code)
+    data_ = data.loc[startdate:enddate]
+    print(code, startdate, enddate)
+
+    x = np.arange(len(data_.index))
+    ohlc = data_[['Open', 'High', 'Low', 'Close']].values
+    dohlc = np.hstack((np.reshape(x, (-1, 1)), ohlc))
+
+    # 봉차트
+    candlestick_ohlc(axes[0], dohlc, width=0.5, colorup='r', colordown='b')
+
+    # 거래량 차트
+    axes[1].bar(x, data_['Volume'], color='grey', width=0.6, align='center')
+    axes[1].set_xticks(range(len(x)))
+    axes[1].set_xticklabels(list(data_.index.strftime('%Y-%m-%d')), rotation=90)
+    axes[1].get_yaxis().set_visible(False)
+
+    plt.tight_layout()
+
+    output = BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route('/pattern', methods=['GET', 'POST'])
+def pattern():
+    if request.method == 'POST':
+        code = request.form['code']
+        startdate = request.form['startdate']
+        enddate = request.form['enddate']
+    else:
+        code  = request.args.get('code', None)
+        startdate  = request.args.get('startdate', None)
+        enddate  = request.args.get('enddate', None)
+    p = pt.PatternFinder()
+    p.set_stock(code)
+    result = p.search(startdate, enddate)
+    N = 5
+    preds = p.stat_prediction(result, period=N)
+
+    if len(preds) > 0:
+        avg_ = preds.mean() * 100
+        min_ = preds.min() * 100
+        max_ = preds.max() * 100
+        size_ = len(preds)
+        print(avg_, min_, max_, size_)
+        return render_template('stock-result.html', code=code, startdate=startdate, enddate=enddate, avg=round(avg_, 2), min=round(min_, 2), max=round(max_, 2), size=size_)
+    else:
+        return render_template('stock-result.html', code=code, startdate=startdate, enddate=enddate, noresult=1)
 
 if __name__ == '__main__':
     # app.run(debug=True, port=5001)
